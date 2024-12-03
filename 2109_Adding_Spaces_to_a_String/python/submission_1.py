904a3e2d67a2d84817516015d3cdbf44a617271c
Suriya-KP <120016620+SURIYA-KP@users.noreply.github.com>
2024-12-04 00:51:19 +0530

    slight change to submission_1

--- 2109_Adding_Spaces_to_a_String/python/submission_1.py
+++ 2109_Adding_Spaces_to_a_String/python/submission_1.py
@@ -1,7 +1,7 @@
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
         output = ""
-        output += s[0:spaces[0]]
+        output += s[0: spaces[0]]
         for i in range(1, len(spaces)):
             output += " " + s[spaces[i - 1]: spaces[i]]
         output += " " + s[spaces[-1]:]

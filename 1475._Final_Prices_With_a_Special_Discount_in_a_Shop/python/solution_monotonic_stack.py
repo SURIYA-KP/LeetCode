ad17dfd49646132fffba46e15559dc1f27d61b3e
Suriya-KP <120016620+SURIYA-KP@users.noreply.github.com>
2024-12-18 23:13:12 +0530

    Solved!

---
+++ 1475._Final_Prices_With_a_Special_Discount_in_a_Shop/python/solution_monotonic_stack.py
@@ -0,0 +1,11 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        m_stack = []
+        for i in range(len(prices) - 1, -1, -1):
+            price = prices[i]
+            while(m_stack and m_stack[-1] > price):
+                m_stack.pop()
+            if m_stack:
+                prices[i] -= m_stack[-1]
+            m_stack.append(price)
+        return prices
